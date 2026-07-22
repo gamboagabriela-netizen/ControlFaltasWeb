de9cb8ca3f2ff7c8a316c32e16f422174fb75d56
gamboagabriela-netizen <quincozgamboalauragabriela@gmail.com>
2026-07-21 20:36:56 -0500

    Configuración para Render

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0d8d!9%^2$ony5@wo=%1hlm455d%dd3w+)t3ptyc$u#tv)wz1+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -115,7 +115,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = "/media/"
 
